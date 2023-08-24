4f67ce5cd6012e72c7b624f161c8a7ff12b8181e
brunaanunciacao <bruna.anunciacao@ufba.br>
2023-08-24 20:35:28 -0300

    Inicialização do Sistema

--- index.js
+++ index.js
@@ -5,4 +5,22 @@ const chalk = require('chalk');
 // modulos internos
 const fs = require('fs');
 
-console.log("Iniciando")
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+            'Criar uma conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ],
+    }]).then((answer) => {
+        const action = answer['action'];
+        
+    }).catch((err) => console.log(err));
+}
+
+operation()
